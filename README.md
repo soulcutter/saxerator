b4bebbdbb5d9aab95b04d7dd17dd426223d1db86
Bradley Schaefer <bradley.schaefer@gmail.com>
2012-04-07 00:47:08 -0500

    Added more documentation, started to write more specs

--- README.md
+++ README.md
@@ -13,3 +13,34 @@ Saxerator.parser(File.new("rss.xml")).for_tag(:item).each do |item|
     puts "#{item['title']}: #{item['author']"
 end
 ```
+
+Compatibility
+-------------
+This library is known to work with the following rubies:
+
+* MRI 1.9.3-p125
+* JRuby 1.6.7 (with JRUBY_OPTS=--1.9)
+
+Saxerator may work with other versions with support for Fiber.
+
+FAQ
+---
+* Why the name 'Saxerator'?
+
+  > It's a combination of SAX + Enumerator.
+
+* Why use Saxerator over regular SAX parsing?
+
+  > Much of the SAX parsing code I've written over the years has fallen into a pattern that Saxerator encapsulates:
+  > marshall a chunk of an XML document into an object, operate on that object, then move on to the
+  > next chunk. Saxerator alleviates the pain of marshalling and allows you to focus solely on operating on the
+  > document chunk.
+
+* Why not DOM parsing?
+
+  > DOM parsers load the entire document into memory. Saxerator only holds a single chunk in memory at a time. If your
+  > document is very large, this can be an important consideration.
+
+### Acknowledgements ###
+Saxerator was inspired by [Nori](https://github.com/rubiii/nori) and [Gregory Brown](http://majesticseacreature.com/)'s
+[Practicing Ruby](http://practicingruby.com/)
--- spec/lib/saxerator_spec.rb
+++ spec/lib/saxerator_spec.rb
@@ -1,14 +1,37 @@
 require 'spec_helper'
 
 describe Saxerator do
+  def fixture_file(name)
+    File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', name))
+  end
+
   context ".parser" do
     subject { parser }
     let(:parser) { Saxerator.parser(xml) }
 
-    context "with a string" do
+    context "with a string with blurbs" do
       let(:xml) { "<blurbs><blurb>one</blurb><blurb>two</blurb><blurb>three</blurb></blurbs>" }
 
-      it { should be }
+      it "should parse simple strings" do
+        results = []
+        subject.for_tag(:blurb).each { |x| results << x }
+        results.should == ['one', 'two', 'three']
+      end
+
+      context "and one non-blurb" do
+        let(:xml) { "<blurbs><blurb>one</blurb><blurb>two</blurb><blurb>three</blurb><notablurb>four</notablurb></blurbs>" }
+        it "should only parse the requested tag" do
+          results = []
+          subject.for_tag(:blurb).each { |x| results << x }
+          results.should == ['one', 'two', 'three']
+          subject.for_tag(:notablurb).each { |x| results << x }
+          results.should == ['one', 'two', 'three', 'four']
+        end
+      end
+    end
+
+    context "with a file with blurbs" do
+      let(:xml) { fixture_file('flat_blurbs.xml') }
 
       it "should parse simple strings" do
         results = []
@@ -17,15 +40,13 @@ describe Saxerator do
       end
     end
 
-    context "with a file" do
-      let(:xml) { File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'flat_blurbs.xml')) }
+    context "with a file with nested elements" do
+      let(:xml) { fixture_file('nested_elements.xml') }
 
-      it { should be }
-
-      it "should parse simple strings" do
-        results = []
-        subject.for_tag(:blurb).each { |x| results << x }
-        results.should == ['one', 'two', 'three']
+      it "should give access to nested elements" do
+        pending do
+          subject.for_tag(:entry).first['title'].should == 'How to eat an airplane'
+        end
       end
     end
   end
