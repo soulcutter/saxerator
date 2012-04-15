2460c172ea1fb751928025bc8f7c346d2950292a
Bradley Schaefer <bradley.schaefer@gmail.com>
2012-04-14 21:16:08 -0500

    Added benchmarks for different parsing predicates

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -9,10 +9,15 @@ if !File.exists?(file)
 end
 file = File.new(file)
 
-tag = ARGV.shift || :artist
-
-count = 0
+count = count2 = count3 = count4 = 0
 Benchmark.bm do |x|
-  x.report { Saxerator.parser(file).for_tag(tag).each { count = count + 1 } }
+  x.report('for_tag') { Saxerator.parser(file).for_tag(:artist).each { count = count + 1 } }
+  x.report('at_depth') { Saxerator.parser(file).at_depth(2).each { count2 = count2 + 1 } }
+  x.report('within') { Saxerator.parser(file).within(:artists).each { count3 = count3 + 1 } }
+  x.report('composite') { Saxerator.parser(file).for_tag(:name).within(:artist).at_depth(3).each { count4 = count4 + 1} }
 end
-puts "#{count} #{tag} elements parsed"
+
+puts "for_tag:   #{count} artist elements parsed"
+puts "at_depth:  #{count2} elements parsed"
+puts "within:    #{count3} artists children parsed"
+puts "composite: #{count4} names within artist nested 3 tags deep parsed"
