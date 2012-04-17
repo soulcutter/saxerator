1dbf1136685c30c843e2331d58c8ae861aba1bdf
Bradley Schaefer <bradley.schaefer@gmail.com>
2012-04-16 23:55:51 -0500

    I believe File globbing is no good for compiled gems. Woops?

--- lib/saxerator.rb
+++ lib/saxerator.rb
@@ -1,8 +1,18 @@
 require 'nokogiri'
 
-Dir.glob('lib/saxerator/**/*.rb').each do |f|
-  require f[4..-1]
-end
+require 'saxerator/version'
+
+require 'saxerator/full_document'
+require 'saxerator/document_fragment'
+require 'saxerator/string_with_attributes'
+require 'saxerator/hash_with_attributes'
+require 'saxerator/xml_node'
+
+require 'saxerator/parser/accumulator'
+require 'saxerator/parser/element_name_latch'
+require 'saxerator/parser/depth_latch'
+require 'saxerator/parser/within_element_latch'
+require 'saxerator/parser/latched_accumulator'
 
 module Saxerator
   extend self
