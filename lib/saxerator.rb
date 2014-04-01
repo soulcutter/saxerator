9d586c3037a10520ef731d07e3b4c80a75c8b826
Bradley Schaefer <bradley.schaefer@gmail.com>
2014-04-01 00:35:45 -0500

    Add EmptyElement concept to HashBuilder
    This should allow code dealing with sometimes-empty
    string-like elements to require less special casing.

--- lib/saxerator.rb
+++ lib/saxerator.rb
@@ -8,9 +8,10 @@ require 'saxerator/document_fragment'
 require 'saxerator/configuration'
 
 require 'saxerator/builder'
-require 'saxerator/builder/string_element'
-require 'saxerator/builder/hash_element'
 require 'saxerator/builder/array_element'
+require 'saxerator/builder/empty_element'
+require 'saxerator/builder/hash_element'
+require 'saxerator/builder/string_element'
 require 'saxerator/builder/hash_builder'
 require 'saxerator/builder/xml_builder'
 
---
+++ lib/saxerator/builder/empty_element.rb
@@ -0,0 +1,18 @@
+require 'saxerator/builder/hash_element'
+
+module Saxerator
+  module Builder
+    class EmptyElement < HashElement
+      def nil?; true end
+      def !; true end
+
+      def to_s
+        StringElement.new('', name, attributes)
+      end
+
+      def to_h
+        HashElement.new(name, attributes)
+      end
+    end
+  end
+end
